cc21a12c09715c0d77305b2d6c84279b63c947af
miguelbonifaz <miguelbonifaz126@gmail.com>
2023-03-23 21:23:04 -0500

    improvements

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,12 @@ composer install --no-interaction --prefer-dist --optimize-autoloader
 echo "" | sudo -S service php8.2-fpm reload
 
 npm ci
-npm run prod
+npm run build
 
 php artisan route:cache
 php artisan view:clear
 php artisan migrate --force
+php artisan horizon:publish
 php artisan horizon:terminate
 
 echo "🚀 Application deployed!"
